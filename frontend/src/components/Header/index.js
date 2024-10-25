01d35cceed3a648195b870d49fa1551acb167f61
Julio Monteiro <julioalexsandro.monteiro13@gmail.com>
2024-10-25 17:04:12 -0300

    Atualizações

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -2,6 +2,7 @@ import OpcoesHeader from '../OpcoesHeader'
 import Logo from '../Logo'
 import IconesHeader from '../IconesHeader';
 import styled from 'styled-components'
+import { Link } from 'react-router-dom';
 
 const HeaderContainer = styled.header`
     display: flex;
@@ -13,10 +14,12 @@ const HeaderContainer = styled.header`
 function Header(){
     return(
         <HeaderContainer>
-      <Logo/>
-      <OpcoesHeader/>
-      <IconesHeader/>
-      </HeaderContainer>
+            <Link to="/">
+            <Logo/>
+            </Link>
+            <OpcoesHeader/>
+            <IconesHeader/>
+        </HeaderContainer>
     )
 }
 
