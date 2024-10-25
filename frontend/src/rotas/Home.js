3815cbf3d09b9320416a2074907835039ff8d5a1
Julio Monteiro <julioalexsandro.monteiro13@gmail.com>
2024-10-25 17:04:21 -0300

    Create Home.js

---
+++ frontend/src/rotas/Home.js
@@ -0,0 +1,22 @@
+import Pesquisa from '../components/Pesquisa';
+import styled from 'styled-components';
+import UltimosLancamentos from '../components/UltimosLancamentos'
+
+const AppContainer = styled.div`
+  
+    width: 100vw;
+    height: 100vh;
+    background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
+`
+
+function Home() {
+  return (
+    <AppContainer>
+      <Pesquisa />
+      <UltimosLancamentos/>
+    </AppContainer>
+  );
+}
+
+export default Home;
+ 
