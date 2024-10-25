1a762eb7bb880371d1a8fd7212425576c188ccce
Julio Monteiro <julioalexsandro.monteiro13@gmail.com>
2024-10-25 17:05:27 -0300

    Rotas favoritos

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,18 @@
 const express = require("express")
 const rotaLivro = require("./routers/livro")
+const rotaFavorito = require("./routers/favorito")
 const rotaExemplos = require("./routers/exemplos")
+const cors = require("cors")
 
 const app = express()
 app.use(express.json())
+app.use(cors({origin: "*"}))
 
 const port = 8000
 
 app.use("/livros", rotaLivro)   
+
+app.use('/favoritos', rotaFavorito)
 
 app.use('/exemplos', rotaExemplos)
  
